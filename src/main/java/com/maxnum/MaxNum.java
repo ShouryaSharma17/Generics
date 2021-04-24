b716d5300a99c74093f523642c0b93d2dba47073
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-04-24 21:41:07 +0530

    Method To Take More Than Three Parameters

--- src/main/java/com/maxnum/MaxNum.java
+++ src/main/java/com/maxnum/MaxNum.java
@@ -1,23 +1,34 @@
 package com.maxnum;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MaxNum <E extends Comparable> {
     E firstNum;
     E secondNum;
     E thirdNum;
-    public MaxNum( E firstNum, E secondNum, E thirdNum){
+    E fourthNum;
+    E fifthNum;
+    public MaxNum( E firstNum, E secondNum, E thirdNum, E fourthNum, E fifthNum){
         this.firstNum=firstNum;
         this.secondNum=secondNum;
         this.thirdNum=thirdNum;
+        this.fourthNum=fourthNum;
+        this.fifthNum=fifthNum;
     }
     public E genericMax(){
-        return findMaximumGeneric(firstNum, secondNum, thirdNum);
+        return findMaximumGeneric(firstNum, secondNum, thirdNum, fourthNum, fifthNum);
     }
-    public static <E extends Comparable> E findMaximumGeneric( E firstNum, E secondNum, E thirdNum){
-        E position= firstNum;
-        if( secondNum.compareTo(position) > 0)
-            position= thirdNum;
-        if( thirdNum.compareTo(position) > 0)
-            position= secondNum;
-        return position;
+    public static <E extends Comparable> E findMaximumGeneric( E firstNum, E secondNum, E thirdNum, E fourthNum, E fifthNum){
+        List<E> testList = new ArrayList<>();
+            testList.add(firstNum);
+            testList.add(secondNum);
+            testList.add(thirdNum);
+            testList.add(fourthNum);
+            testList.add(fifthNum);
+
+        Collections.sort(testList);
+        return testList.get(5);
     }
+
 }
--- src/test/java/com/maxnum/MaxNumTest.java
+++ src/test/java/com/maxnum/MaxNumTest.java
@@ -7,21 +7,21 @@ public class MaxNumTest {
     @Test
     // UC1, TC 1.1 Test for checking greatest number at first position
     public void MaxNumberWhenGenericPassed() {
-        testObject= new MaxNum(28,13,21);
+        testObject= new MaxNum(28,13,21,10,5);
         Integer testMax = (Integer) testObject.genericMax();
         Assert.assertEquals(28,(int) testMax);
     }
      @Test
      // UC2 Checking Float values
         public void FindMaxGenericFloat() {
-         testObject= new MaxNum(15.2,15.3,15.4);
+         testObject= new MaxNum(15.2,15.3,15.4,15.1,15.11);
          Double testMaximum = (Double) testObject.genericMax();
          Assert.assertEquals(15.4, testMaximum, 0);
      }
      @Test
      //UC3 Checking String Value
         public void FindMaxGenericString() {
-        testObject= new MaxNum("Apple","Peach","Banana");
+        testObject= new MaxNum("Apple","Peach","Banana","Pea","Pear");
         String testMaximum = (String) testObject.genericMax();
         Assert.assertEquals("Banana", testMaximum);
     }
